7a0a1c471f98481e79795e80fdfdf78d7e290d3c
Minna <Minna.Yao@gmail.com>
2013-05-06 14:26:00 +1000

    create a git project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BneOfficeOrderSystem::Application.config.secret_token = '0bd0a029f5922c60d1fd5213b93547adcd46928de13e76bdb6f7168a6ca858327c05c0b8ef6aa27a70d1e36f81633fc2bda4e53ef2970e13101eb4a2c3eeda70'
